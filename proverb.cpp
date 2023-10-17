fda9f21265ca7f4ce1aec7e048fae2408b84c0ca
jonwynn <143842244+jonwynn@users.noreply.github.com>
2023-10-17 17:24:36 -0500

    Add files via upload

--- proverb.cpp
+++ proverb.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 using namespace std;
-// Lab 6.2
-// 
+// Lab 6.1
 // Jonathan Wynn
 
-//Exercise 1: If you enter in -3.97, the program still executes properly due to the else statement inside of the writeProverb function 
-//catching any numbers that are not 1 and then printing "Now is the time for all good men to come to the aid of their country."
-
-
-
-void writeProverb(string code)
-{
-    code = wordCode;
-    cout << "Now is the time for all good men to come to the aid of their " << code << "." << endl;
-    
+void writeProverb(); // function prototype
 
 
 int main()
 {
-    int wordCode;
-    cout << "Given the phrase:" << endl;
-    cout << "Now is the time for all good men to come to the aid of their ____" << endl;
-    cout << "Please input the word that you would like to finish the proverb." << endl;
-    cin >> wordCode;
-    writeProverb(wordCode);
+    cout << endl;
+    writeProverb();
+    cout << endl;
 }
 
 // *********************************************************************
 // writeProverb
 //
-// task: This function prints a proverb. The function takes a number
-// from the call. If that number is a 1 it prints "Now is the time
-// for all good men to come to the aid of their party."
-// Otherwise, it prints "Now is the time for all good men to come to the
-// aid of their country."
+// task: This function prints a proverb
+// data in: none
+// data out: none
+//
 // ********************************************************************
-
+void writeProverb()
+{
+    cout << "Now is the time for all good men to come to the aid of their party.";
+}
