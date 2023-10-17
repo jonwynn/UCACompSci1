9bf87cf3f45bc5c71627b3dab62aa10e81f3c826
jonwynn <143842244+jonwynn@users.noreply.github.com>
2023-10-17 17:09:01 -0500

    Add files via upload

---
+++ proverb.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+// Lab 6.1
+// Jonathan Wynn
+
+void writeProverb(); // function prototype
+
+
+int main()
+{
+    cout << endl;
+    writeProverb();
+    cout << endl;
+}
+
+// *********************************************************************
+// writeProverb
+//
+// task: This function prints a proverb
+// data in: none
+// data out: none
+//
+// ********************************************************************
+void writeProverb()
+{
+    cout << "Now is the time for all good men to come to the aid of their party.";
+}
---
+++ yourchoice.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+using namespace std;
+
+// Lab 6.4
+// Jonathan Wynn
+
+int main()
+{
+
+}
